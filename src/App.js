d7a87601459329534e695b1e5fc835e693464740
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-04-14 16:48:59 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import About from './About/About';
 import './App.css';
+import Contacts from './Contacts/Contacts';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <a href={ <About />}></a>
+      <a href={ <Contacts />}></a>
+      <a href={ <Home />}></a>
+    
+     
     </div>
   );
 }
