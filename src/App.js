b37b856f889181bb8acfd8d1e56adfc18736d4fd
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-04-14 22:06:01 +0600

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import About from './About/About';
 import './App.css';
-import Contacts from './Contacts/Contacts';
-import Home from './Home/Home';
+import About from "./About/About"
+import Contacts from "./Contacts/Contacts"
+import Home from "./Home/Home"
+import { Route } from "react-router"
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
-      <a href={ <About />}></a>
-      <a href={ <Contacts />}></a>
-      <a href={ <Home />}></a>
-    
      
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contakts">Contacts</Link>
+      
+       
+      <Route path="/about" component = {About}/>
+      <Route path="/contakts" component = {Contacts}/>
+      <Route path="/" component = {Home} exact/>
+     
+      
     </div>
   );
 }
