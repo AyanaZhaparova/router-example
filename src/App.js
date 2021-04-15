50522915a400450751cceda90ecd74a5a02a40c4
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-04-15 13:55:00 +0600

    changed App.js

--- src/App.js
+++ src/App.js
@@ -16,9 +16,8 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
-     <ul className="nav">
-
+      <nav>
+     <ul>
       <Link to="/new"><li>NEW</li></Link>
       <Link to="/home"><li>Home</li></Link>
       <Link to="/catalog"><li>Catalog</li></Link>
@@ -26,9 +25,9 @@ function App() {
       <Link to="/about"><li>About</li></Link>
       <Link to="/contacts"><li>Contacts</li></Link>
       <Link to="/auth"><li>Auth</li></Link>
-      
       </ul>
-      
+      </nav>
+
       <Route path="/new" component = {New} />
       <Route path="/home" component = {Home} />
       <Route path="/catalog" component = {Catalog} />
