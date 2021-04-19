9a86ed14c7cd01f2449f54dbdd50e4e49ae6a180
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-04-19 08:33:54 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import './App.css';
 
 import About from "./About/About";
+import Auth from "./Auth/Auth";
+import Bestsellers from "./Bestsellers/Bestsellers";
+import Catalog from "./Catalog/Catalog";
 import Contacts from "./Contacts/Contacts";
 import Home from "./Home/Home";
-import Catalog from "./Catalog/Catalog";
-import Sale from "./Sale/Sale";
-import New from "./New/New";
-import Auth from "./Auth/Auth";
-import Posts from "./Posts/Posts";
+import LookBook from "./LookBook/LookBook";
 import Nav from "./Nav/Nav";
+import New from "./New/New";
+import Posts from "./Posts/Posts";
+import Sale from "./Sale/Sale";
+import Soon from "./Soon/Soon";
+import Dresses from "./Clothes/Dresses/Dresses";
+import Suits from "./Clothes/Suits/Suits";
 
 import { Route, Switch } from "react-router";
-
-
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      
       <div className="content">
      <Switch>
-      <Route path="/" component = {Home} exact/> 
-      <Route path="/new" component = {New} />
-      <Route path="/catalog" component = {Catalog} />
-      <Route path="/sale" component = {Sale} />
       <Route path="/about" component = {About}/>
-      <Route path="/contacts" component = {Contacts}/>
       <Route path="/auth" component = {Auth}/>
+      <Route path="/bestsellers" component = {Bestsellers}/>
+      <Route path="/catalog" component = {Catalog} />
+      <Route path="/contacts" component = {Contacts}/>
+      <Route path="/" component = {Home} exact/> 
+      <Route path="/lookbook" component = {LookBook}/>
+      <Route path="/new" component = {New} />
       <Route path="/posts" component = {Posts}/>
+      <Route path="/sale" component = {Sale} />
+      <Route path="/soon" component = {Soon}/>
+      <Route path="/dresses" component = {Dresses}/>
+      <Route path="/suits" component = {Suits}/>
+
       <Route path="/" render={() => <h1>Page not found</h1>} />
      
       </Switch>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,24 @@
 import classes from "./Home.module.css";
 
+import image1 from "./../images/image1.jpg";
+import image2 from "./../images/image2.jpg";
+import image3 from "./../images/image3.jpg";
+
+import { Link } from "react-router-dom";
+
+
 const Home = () => {
     return (
+        
         <div className={classes.Home}>
-            <h1>Home</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <Link className={classes.News} to="/new">NEW</Link>
+
+            <div className={classes.header}>
+            <Link to="/suits"><img src={image1}/></Link>
+            <Link to="/dresses"><img src={image2}/></Link>
+            <Link to="/bestsellers"><img src={image3}/></Link>
+            
+            </div>
         </div>
     );
 }
